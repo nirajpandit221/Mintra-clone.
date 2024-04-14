3f0a214f69909edbea3240b5d69fcd91001e9a09
NIRAJ PANDIT <132039683+nirajpandit221@users.noreply.github.com>
2024-04-14 12:18:17 +0530

    Add files via upload

---
+++ Mintra Clone/media/8.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+	int a,b, result;
+		printf("pleas enter the number:");
+		scanf("%d%d, &a, &b");
+		result = a+b;
+		printf("the sum of the number is ; %d",result);
+		
+}
---
+++ Mintra Clone/media/9.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+	int a,b, result;
+		printf("pieas enter the number:");
+		scanf("%d%d, &a, &b");
+		result = a+b;
+		printf("the sum of the number is ; %d",result);
+		
+}
